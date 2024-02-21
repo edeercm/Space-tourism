2e70c6ef507fdb70d94fe5698ea58fa01374b032
Eder <132967872+edeercm@users.noreply.github.com>
2024-02-21 03:51:43 -0600

    Update

--- src/components/TechComps/TechData.jsx
+++ src/components/TechComps/TechData.jsx
@@ -10,24 +10,24 @@ export default {
     {
       "name": "Launch vehicle",
       "image": {
-        desktop : launch,
-        mobile : launchlandscape
+        "desktop" : launch,
+        "mobile" : launchlandscape
       },
       "description": "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
     },
     {
       "name": "Spaceport",
       "image": {
-        desktop : spaceport,
-        mobile : spaceportlandscape
+        "desktop" : spaceport,
+        "mobile" : spaceportlandscape
       },
       "description": "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
     },
     {
       "name": "Space capsule",
       "image": {
-        desktop : space,
-        mobile : spacelandscape
+        "desktop" : space,
+        "mobile" : spacelandscape
       },
       "description": "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
     }
